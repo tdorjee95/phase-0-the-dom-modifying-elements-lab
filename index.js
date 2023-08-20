f3da29e7220afaa08cef617649de3a5e05968720
Tenzin Dorjee <tenzindorjee95@yahoo.com>
2023-08-20 03:10:10 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+document.getElementById('main').remove();
+// here we grabbed the main element by id
+// and removed it using .remove();
+
+const newHeader = document.createElement('h1');
+// here we set a newHeader variable to a new
+// h1 element in the document
+
+newHeader.id = 'victory';
+// now we can refence that newHeader variable
+// and assign it an id and more
+
+newHeader.textContent = "YOUR-NAME is the champion";
+// since newHeader points to the h1 we created, we can 
+// assign textContent to it and more
